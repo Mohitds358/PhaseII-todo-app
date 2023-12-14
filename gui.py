2c90b023fc8c728553e088c0033b0d3f9afbee3c
Mohit <mohithotwanids@gmail.com>
2023-12-14 19:04:44 +0530

    Implement the Edit button

--- gui.py
+++ gui.py
@@ -4,10 +4,14 @@ import PySimpleGUI as pg
 label = pg.Text("Type in to-do app")
 input_box = pg.InputText(tooltip="Enter Todo", key="todo")
 add_button = pg.Button("Add")
+list_box = pg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[44,10])
+edit_button = pg.Button("Edit")
 
 
 window = pg.Window('My to-do App', layout=[[label],
-                                           [input_box, add_button]],
+                                           [input_box, add_button],
+                                           [list_box, edit_button]],
                    font=('Helvetica', 16))
 
 while True:
@@ -18,9 +22,24 @@ while True:
     match event:
         case 'Add':
             todos = functions.get_todos()
-            new_todo = "\n" + values['todo']
+            new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todo(todos)
+            window['todos'].update(values=todos)
+        case 'Edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+
+            functions.write_todo(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+        case pg.WIN_CLOSED:
+            break
 
 window.close()
 
